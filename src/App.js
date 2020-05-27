b147e3644a7ed837b23e7861b0c0b59aded0cd19
mdestafadilah <desta.08b@gmail.com>
2020-05-27 16:01:53 +0700

    firs shoping komponen

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shooping Komponen
         </a>
       </header>
     </div>
