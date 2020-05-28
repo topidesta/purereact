eef44b4cc58ebaf4cc572ed7ecf0d292aa35fefd
mdestafadilah <desta.08b@gmail.com>
2020-05-28 12:33:50 +0700

    Example Done!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+function MyThing() {
+  return (
+    <>
+      {/* 1 */}
+      <div className="book">
+        <div className="title">The Title</div>
+        <div className="author">The Author</div>
+        <ul className="stats">
+          <li className="rating">5 Starts</li>
+          <li className="isbn">12-3456780910</li>
+        </ul>
+      </div>
+
+      {/* 2a (Single Line) */}
+      <div>NewLine Test</div>
+
+      {/* 2b (Empty newlins) */}
+      <div>Empty NewLines Here</div>
+
+      {/* 2c (Spaces with newlines) */}
+      <div>&nbsp;Non-breaking &nbsp;Spaces&nbsp;</div>
+
+      {/* Inserting spaces when content spans multiple lines */}
+      <div>
+        Line1
+        {"  "}
+        Line2
+      </div>
+    </>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <MyThing />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
