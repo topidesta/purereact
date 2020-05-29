27d58cd41cb55a0721526f5991f2b1979ce57e6e
mdestafadilah <desta.08b@gmail.com>
2020-05-29 13:14:28 +0700

    Setup For Next #props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+function Avatar() {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/nothing"
+      className="avatar"
+      alt="avatar"
+    />
+  );
+}
+
+function Message() {
+  return <div className="message">Ini Pesan 140 karakter</div>;
+}
+
+function Author() {
+  return (
+    <span className="author">
+      <span className="name">Desta</span>
+      <span className="handle">@emang_dasar</span>
+    </span>
+  );
+}
+
+function App() {
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <Author /> <Time /> <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
