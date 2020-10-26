bfe419adda3fd5b5d2546cd5c53bc4b34eebeaa9
Desta F Muchamad <desta.08b@gmail.com>
2020-10-27 02:50:05 +0700

    First Mockup :P

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
+import './index.css';
+
+// Object
+const FileList = ({ files }) => (
+  <table className="file-list">
+    <tbody>
+      {files.map(file => (
+        <tr className="file-list-item" key={file.id}>
+          <td className="file-name">{file.name}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+FileList.propTypes = {
+  files: PropTypes.array
+}
+
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: '2020-07-11 21:24:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'test',
+    type: 'folder',
+    updated_at: '2020-10-11 21:24:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'readme',
+    type: 'file',
+    updated_at: '2020-12-11 21:24:00',
+    latestCommit: {
+      message: 'Added File'
+    }
+  }
+]
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FileList files={testFiles} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
