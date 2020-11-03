06ca71a6c083607ba78e33b52210f0fc0fd99bba
Desta F Muchamad <desta.08b@gmail.com>
2020-11-04 06:10:20 +0700

    Time Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
+import Time from './Time'
 
 // Object
 const FileList = ({ files }) => (
@@ -46,6 +47,9 @@ const FileListItem = ({ file }) => (
   <tr className="file-list-item">
     <FileName file={file} />
     <CommitMessage commit={file.latestCommit} />
+    <td className="age">
+      <Time time={file.updated_at} />
+    </td>
   </tr>
 )
 
